1c667a39ec37c396e4d9118c8af02fb8e7b6f77e
Trần Thanh Mẫn <62742886+tranthanhman1912@users.noreply.github.com>
2021-12-11 09:36:28 +0700

    Migration Cua Man

---
+++ database/migrations/2021_12_11_021844_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->unsignedBigInteger('Payment_id');
+            $table->unsignedBigInteger('User_id');
+            $table->unsignedBigInteger('Voucher_id');
+            $table->date('TimeBuy');
+            $table->Decimal('TotalPrice');
+            $table->integer('Status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
