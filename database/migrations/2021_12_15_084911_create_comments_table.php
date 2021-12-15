42d91ce90bfbf19871cb8ddedd4fc2bff0636f8c
DangKhoa <ndkhoaxxi@gmail.com>
2021-12-15 16:16:40 +0700

    migration_Khoa

---
+++ database/migrations/2021_12_15_084911_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('Rate');
+            $table->string('Evaluate');
+            $table->unsignedBigInteger('Product_id');
+            $table->unsignedBigInteger('User_id');
+            $table->boolean('Status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
